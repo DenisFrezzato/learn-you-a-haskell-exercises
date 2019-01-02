41cb057c14978cb7fd2f583bf1917be9206e69f5
DenisFrezzato <frezzato.denis@gmail.com>
2019-01-02 19:41:58 +0100

    06: Higher order functions.

--- 06-higher-order-functions.hs
+++ 06-higher-order-functions.hs
@@ -2,33 +2,35 @@
 -- Example: sumInts 0 1 = 1
 --          sumInts 1 3 = 6
 sumInts :: Int -> Int -> Int
-sumInts a b = undefined
+sumInts a b = if a <= b then a + sumInts (a + 1) b else 0
 
 -- Define a square function
 sq :: Int -> Int
-sq x = undefined
+sq x = x * x
 
 -- Sum the squares between two numbers. This function should be similar to the sumInts function
 sumSquares :: Int -> Int -> Int
-sumSquares a b = undefined
+sumSquares a b = if a <= b then a * a + sumSquares (a + 1) b else 0
 
 -- Define a higher order sum function which accepts an (Int -> Int) function to apply to all integers between two values.
 -- Again this should look similar to the sumInts and sumSquares functions
 higherOrderSum :: (Int -> Int) -> Int -> Int -> Int
-higherOrderSum intApplication a b = undefined
+higherOrderSum intApplication a b = if a <= b
+    then intApplication a + higherOrderSum intApplication (a + 1) b
+    else 0
 
 -- Define the square sum in terms of higherOrderSum
 hoSumSquares :: Int -> Int -> Int
-hoSumSquares = undefined
+hoSumSquares = higherOrderSum sq
 
 -- Define the sum between two values in terms of higherOrderSum
 -- Note there is no parameter on the function definition
 -- Try to use a lambda if possible
 hoSumInts :: Int -> Int -> Int
-hoSumInts = undefined
+hoSumInts = higherOrderSum (\n -> n)
 
 -- Create a new higher order method which generalises over the function provided by sumInts (That is, parameterize (+) :: Int -> Int -> Int) between a and b
--- This will give the ability to perform utilities such as the prodcut of all squares (or any other Int -> Int function) between a and b
+-- This will give the ability to perform utilities such as the product of all squares (or any other Int -> Int function) between a and b
 -- You will also need to generalise the base case
 -- You can also define the function signature yourself, which leaves you free to define the parameters and their order
 -- To be clear, your function will need to handle:
@@ -37,8 +39,11 @@ hoSumInts = undefined
 --  - A function to apply to each value, op :: Int -> Int
 --  - A function to apply between each value, f :: Int -> Int -> Int
 --  - A value to return in the base case when a > b, z :: Int
-higherOrderSequenceApplication = undefined
+higherOrderSequenceApplication :: (Int -> Int) -> (Int -> Int -> Int) -> Int -> Int -> Int -> Int
+higherOrderSequenceApplication op f z a b = if a <= b
+    then f (op a) (higherOrderSequenceApplication op f z (a + 1) b)
+    else z
 
--- Define a factorial method using the higherOrderSequenceAppliction
+-- Define a factorial method using the higherOrderSequenceApplication
 hoFactorial :: Int -> Int
-hoFactorial = undefined
+hoFactorial = higherOrderSequenceApplication (\n -> n) (*) 1 2
