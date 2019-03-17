eef9888d87349cb6efedb9ac1977b0d6cbc2cb93
DenisFrezzato <frezzato.denis@gmail.com>
2019-03-17 19:07:41 +0100

    11: Functors, Applicative functors and monoid.

--- 11-functors-applicative-functors-and-monoids.hs
+++ 11-functors-applicative-functors-and-monoids.hs
@@ -2,28 +2,66 @@ import Control.Applicative
 import Data.Monoid
 
 -- We can use the following type to simulate our own list
-data List a = Empty | Value a (List a) deriving (Show)
+data List a = Empty | Value a (List a) deriving (Show, Eq)
 
 -- Make the list a Functor
+instance Functor List where
+    fmap _ Empty = Empty
+    fmap f (Value a as) = Value (f a) (fmap f as)
 
 -- Write a function which appends one list on to another
 combineLists:: List a -> List a -> List a
-combineLists a b = undefined
+combineLists Empty as = as
+combineLists as Empty = as
+combineLists (Value head tail) bs = Value head (combineLists tail bs)
 
 -- Make our list a Monoid
+-- instance Monoid (List a) where
+--     mempty = Empty
+--     mappend = combineLists
 
 -- Make our list an Applicative
+instance Applicative List where
+    pure a = Value a Empty
+    Empty <*> _ = Empty
+    (Value head tail) <*> bs = combineLists (fmap head bs) (tail <*> bs)
 
 -- Make sure that the List obeys the laws for Applicative and Monoid
+list1 :: List Integer
+list1 = Value 666 Empty
+list2 :: List Integer
+list2 = Value 111 Empty
+list3 :: List Integer
+list3 = Value 777 Empty
+list4 = Value (-1) Empty
 
--- Create some lists of numbers of different lengths such as:
+applicativeLaw1 = (pure id <*> list1) == list1 
+
+-- applicativeLaw2 = (pure (.) <*> list4 <*> list3 <*> list1) == (list4 <*> (list3 <*> list1))
+
+listPure :: x -> (List x)
+listPure = pure
+-- applicativeLaw3 = (pure (*6) <*> pure 1) == (pure ((*6) 1))
+
+const7 :: Integer
+const7 = 7
+-- applicativeLaw4 = (list4 <*> pure const7) == (pure ($ const7) <*> list4)
+
+-- -- Create some lists of numbers of different lengths such as:
 twoValueList = Value 10 $ Value 20 Empty
+threeValueList = Value 6 $ Value 5 $ Value 53 Empty
 
--- Use <$> on the lists with a single-parameter function, such as:
+-- -- Use <$> on the lists with a single-parameter function, such as:
 plusTwo = (+2)
 
--- Use <$> and <*> on the lists with a binary function
+resPlusTwo = plusTwo <$> twoValueList
+resPlusTwo' = plusTwo <$> threeValueList
 
--- Create some lists of binary functions
+-- -- Use <$> and <*> on the lists with a binary function
+resLiftedSum = (+) <$> twoValueList <*> threeValueList
+
+-- -- Create some lists of binary functions
+binaryFunctionList = combineLists (listPure (+)) $ combineLists (listPure (^)) (listPure (*))
 
 -- Use <*> on the binary functions list and the number lists
+bigNumbers = binaryFunctionList <*> twoValueList <*> threeValueList
